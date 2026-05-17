dd40489907f29a3fd99ab86683231fb4908319c1
Anandi <anandimahajan60@gmail.com>
2026-05-17 12:23:33 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World" << endl;
+
+    cout << "Hello\nWorld" << endl;
+
+    cout << "Hello\tWorld" << endl;
+
+  
+    cout << "Beep sound coming.\a" << endl;
+
+   
+    cout << "Hello World\rHi" << endl;  
+   
+
+    return 0;
+}
